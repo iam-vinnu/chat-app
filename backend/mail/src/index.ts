c0db992f8e26c2f669b05f972fa75768302ca2ef
iam-vinnu <binaybehera101@gmail.com>
2025-08-16 23:58:03 +0530

    not

--- backend/mail/src/index.ts
+++ backend/mail/src/index.ts
@@ -1,7 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import { startSendOtpConsumer } from "./consumer.js";
 const app = express();
-dotenv.config();
+dotenv.config(); 
+  
+
+
+startSendOtpConsumer();
+
 
 app.listen(process.env.PORT , ()=>{
     console.log(`Server is running at ${process.env.PORT}`);
