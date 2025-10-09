9586954a33cd3d1899b5d6d07d50587bff1a9067
iam-vinnu <binaybehera101@gmail.com>
2025-10-09 21:13:39 +0530

    verify page

--- frontend/src/component/VerifyOtp.tsx
+++ frontend/src/component/VerifyOtp.tsx
@@ -1,50 +1,37 @@
-"use client";
-import axios from 'axios';
-import { ArrowRight, Loader2, Mail } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+'use client'
+import { ArrowRight, Loader2, Lock } from 'lucide-react';
+import { useSearchParams } from 'next/navigation';
 import React, { useState } from 'react'
 
-const LoginPage = () => {
-    const [email, setEmail] = useState<string>("");
-    const [loading, setLoading] = useState<boolean>(false);
-    const router = useRouter();
+const VerifyOtp = () => {
+    const [loading,setLoading] = useState(false);
 
-    const handleSubmit = async (e: React.FormEvent<HTMLElement>): Promise<void> => {
-        e.preventDefault();
-        setLoading(true);
-        try {
-            const { data } = await axios.post(`http://localhost:5000/api/v1/login`, { email });
-            alert(data.message);
-            router.push(`/verify?email=${email}`);
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        } catch (error: any) {
-            alert(error.response.data.message);
-        } finally {
-            setLoading(false);
-        }
-    }
-    return (
-        <div className='min-h-screen bg-gray-900 flex items-center justify-center  py-4'>
+    const searchParams = useSearchParams();
+    const email: string = searchParams.get('email') || '';
+    const handleSubmit = async()=>{}
+  return (
+     <div className='min-h-screen bg-gray-900 flex items-center justify-center  py-4'>
             <div className='max-w-md w-full'>
                 <div className="bg-gray-800 border border-gray-700 rounded-lg p-8">
                     <div className='text-center mb-8'>
                         <div className='mx-auto w-20 h-20 bg-blue-600 rounded-lg flex items-center justify-center mb-6'>
-                            <Mail size={40} className='text-white' />
+                            <Lock size={40} className='text-white' />
                         </div>
                         <h1 className='text-2xl font-bold text-white mb-3'>
-                            Welcome To ChatApp
+                            Verify Your Email
                         </h1>
                         <p className='text-gray-300 text-base '>
-                            Enter your email to countinue your journey
+                            We have sent you 6-Digit code to
                         </p>
+                        <p className='text-blue-400 font-medium' >{email}</p>
                     </div>
 
                     <form onSubmit={handleSubmit} className="space-y-6">
                         <div>
                             <label htmlFor="email" className='bold text-sm font-medium text-gray-300 mb-2'>Email Address</label>
                             <input type='email' id='email'
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
+                                // value={email}
+                                // onChange={(e) => setEmail(e.target.value)}
                                 className='w-full px-4 py-4 bg-gray-700 border border-gray-600   rounded-lg text-white placeholder-gray-400'
                                 placeholder='Enter Your Email Address'
                                 required />
@@ -68,7 +55,7 @@ const LoginPage = () => {
                 </div>
             </div>
         </div>
-    )
+  )
 }
 
-export default LoginPage;
+export default VerifyOtp;
